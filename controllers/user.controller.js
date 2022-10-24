5dd97280f24017da155cc8daf55b3c8e188491c6
Aripin2401 <ujangarifin06@gmail.com>
2022-10-24 14:03:43 +0700

    From Content to Konten

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,15 @@
 exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
+    res.status(200).send("Public Konten.");
   };
   
   exports.userBoard = (req, res) => {
-    res.status(200).send("User Content.");
+    res.status(200).send("User Konten.");
   };
   
   exports.adminBoard = (req, res) => {
-    res.status(200).send("Admin Content.");
+    res.status(200).send("Admin Konten.");
   };
   
   exports.moderatorBoard = (req, res) => {
-    res.status(200).send("Moderator Content.");
+    res.status(200).send("Moderator Konten.");
   };
